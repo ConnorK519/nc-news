477f8c3f98520d9cfe92cc771f8c3246900e99e0
connork <connorkelly-95@hotmail.co.uk>
2023-06-26 14:34:16 +0100

    fixed issues

--- app.js
+++ app.js
@@ -1,19 +1,8 @@
 const express = require("express");
 const { getTopics } = require("./controllers/topics.controllers");
-const {
-  handlePsqlErrors,
-  handleCustomErrors,
-  handleServerErrors,
-} = require("./errors/error-handling");
 
 const app = express();
 
 app.get("/api/topics", getTopics);
-
-app.use(handlePsqlErrors);
-
-app.use(handleCustomErrors);
-
-app.use(handleServerErrors);
 
 module.exports = app;
