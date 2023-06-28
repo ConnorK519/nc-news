dce302a94da72c93d6986b45948e7736f095cbde
connork <connorkelly-95@hotmail.co.uk>
2023-06-28 11:47:54 +0100

    quick commit

--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -4,7 +4,8 @@ const {
 } = require("../models/articles.models");
 
 exports.getArticles = (req, res, next) => {
-  return selectArticles().then((articles) => {
+  const { topic, sort_by, order } = req.query;
+  return selectArticles(topic, sort_by, order).then((articles) => {
     res.status(200).send({ articles });
   });
 };
--- models/articles.models.js
+++ models/articles.models.js
@@ -1,19 +1,17 @@
 const db = require("../db/connection");
 const format = require("pg-format");
 
-exports.selectArticles = () => {
-  return db
-    .query(
-      `SELECT articles.author, title, articles.article_id, topic, articles.created_at, articles.votes, article_img_url, COUNT(comment_id) AS comment_count
-    FROM articles 
-    LEFT JOIN comments ON comments.article_id = articles.article_id 
-    GROUP BY articles.article_id
-    ORDER BY articles.created_at DESC
-    `
-    )
-    .then(({ rows }) => {
-      return rows;
-    });
+exports.selectArticles = (topic = "*", sort_by, order) => {
+  let query = `SELECT articles.author, title, articles.article_id, topic, articles.created_at, articles.votes, article_img_url, COUNT(comment_id) AS comment_count
+  FROM articles 
+  LEFT JOIN comments ON comments.article_id = articles.article_id 
+  GROUP BY articles.article_id
+  ORDER BY articles.created_at DESC
+  `;
+
+  return db.query(query).then(({ rows }) => {
+    return rows;
+  });
 };
 
 exports.selectArticleById = (article_id) => {
